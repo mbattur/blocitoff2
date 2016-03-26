3a0bc4fd2334faf1f810bef88d131e53c2c751a0
mbattur <mbattur@yahoo.com>
2016-03-26 16:24:32 +0000

    gravatar rspec passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  
+  describe ".avatar_url" do
+    let(:known_user) { create(:user, email: "blochead@bloc.io") }
+      
+    it "returns the proper Gravatar url for a known email entity" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)  
+    end
+  end
 end
