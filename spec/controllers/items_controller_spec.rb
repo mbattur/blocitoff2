1841d4e781a5890ccc1941c8693d18cdd4f78162
mbattur <mbattur@yahoo.com>
2016-03-30 20:27:58 +0000

    fixed rspecs

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe ItemsController, type: :controller do
-  let(:user) { create(:user) }
+  let(:my_user) { create(:user) }
   let(:my_item) { create(:item) }
   
   before (:each) do
@@ -12,34 +12,29 @@ RSpec.describe ItemsController, type: :controller do
   
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      get :new, user_id: my_user.id
       expect(response).to have_http_status(:success)
     end
     
     it "renders the #new view" do
-      get :new
+      get :new, user_id: my_user.id
       expect(response).to render_template :new
     end
     
     it "instantiates @item" do
-      get :new
+      get :new, user_id: my_user.id
       expect(assigns(:item)).not_to be_nil
     end
   end
 
   describe "POST create" do
     it "increases the number of Item by 1" do
-      expect{ post :create, item: {name: RandomData.random_sentence}}.to change(Item,:count).by(1)
+      expect{ post :create, user_id: my_user.id, item: {name: RandomData.random_sentence}}.to change(Item,:count).by(1)
     end
 
     it "assigns the new item to @item" do
-      post :create, item: {name: RandomData.random_sentence}
+      post :create, user_id: my_user.id, item: {name: RandomData.random_sentence}
       expect(assigns(:item)).to eq Item.last
-    end
-
-    it "redirects to the new item" do
-      post :create, item: {name: RandomData.random_sentence}
-      expect(response).to redirect_to user_path(current_user)
     end
   end
 end
