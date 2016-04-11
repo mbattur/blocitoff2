d41bfe0630c2259809ddcdbee29db3c2d3397903
mbattur <mbattur@yahoo.com>
2016-04-11 22:09:05 +0000

    fixed views

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,10 @@ require 'factory_girl_rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+require 'simplecov'
+SimpleCov.start do 
+  add_filter '/spec/'
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
