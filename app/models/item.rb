d4bef59efce01384ceb7eb63f4546c38f812947a
mbattur <mbattur@yahoo.com>
2016-04-12 14:46:37 +0000

    time checkpoint completed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   
-  def age_in_days
-    (DateTime.now.to_date - created_at.to_date).to_i
-  end
+  # def age_in_days
+  #   (DateTime.now.to_date - created_at.to_date).to_i
+  # end
 
   def days_left
     7 - (DateTime.now.to_date - created_at.to_date).to_i
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   let(:item) { Item.create!(name: "New Item Name") }
- 
-   describe "attributes" do
-     it "responds to name" do
-       expect(item).to respond_to(:name)
-     end
-   end
+  
+  describe "attributes" do
+    it "responds to name" do
+      expect(item).to respond_to(:name)
+    end
+  end
 end
